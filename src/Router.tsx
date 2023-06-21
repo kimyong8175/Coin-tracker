9d00d9c18c331c18265ccdb4d7e7b001008ba77d
kimyong8175 <kimyong8175@naver.com>
2023-06-20 20:14:45 -0400

    done with deploy

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,32 +5,35 @@ import Root from "./Root";
 import Chart from "components/Chart";
 import Price from "components/Price";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
 
-    children: [
-      {
-        path: "",
-        element: <Coins />,
-      },
-      {
-        path: ":coinId",
-        element: <Coin />,
-        children: [
-          {
-            path: "chart",
-            element: <Chart />,
-          },
-          {
-            path: "price",
-            element: <Price />,
-          },
-        ],
-      },
-    ],
-  },
-]);
+      children: [
+        {
+          path: "",
+          element: <Coins />,
+        },
+        {
+          path: ":coinId",
+          element: <Coin />,
+          children: [
+            {
+              path: "chart",
+              element: <Chart />,
+            },
+            {
+              path: "price",
+              element: <Price />,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  { basename: "/coin-tracker" }
+);
 
 export default router;
